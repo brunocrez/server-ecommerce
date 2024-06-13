6f56bbba147c6f6cf9cfdf8e97130daddb31aad4
Bruno Rezende <brunoc.rez@gmail.com>
2024-06-13 12:17:50 -0300

    chore: remove get all controller

--- src/controllers/product-on-group/GetProductsOnGroupController.ts
+++ src/controllers/product-on-group/GetProductsOnGroupController.ts
@@ -1,13 +1,20 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { GetProductsOnGroupService } from '../../services/GetProductsOnGroupService'
+import { GetAllProductsOnGroupService } from '../../services/GetAllProductsOnGroupService'
 
 export class GetProductsOnGroupController {
   async handle(req: FastifyRequest, reply: FastifyReply) {
     const { groupId } = req.query as { groupId: string }
-    const service = new GetProductsOnGroupService()
+    const getProductsService = new GetProductsOnGroupService()
+    const getAllService = new GetAllProductsOnGroupService()
 
     try {
-      const products = await service.execute(Number(groupId))
+      if (groupId) {
+        const products = await getProductsService.execute(Number(groupId))
+        reply.send(products)
+      }
+
+      const products = await getAllService.execute()
       reply.send(products)
     } catch (error) {
       reply.status(500).send({ message: 'Internal Server Error!' })
--- src/routes/products-on-group.route.ts
+++ src/routes/products-on-group.route.ts
@@ -1,15 +1,10 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 import { GetProductsOnGroupController } from '../controllers/product-on-group/GetProductsOnGroupController'
 import { CreateProductOnGroupController } from '../controllers/product-on-group/CreateProductOnGroupController'
-import { GetAllProductsOnGroupController } from '../controllers/product-on-group/GetAllProductsOnGroupController'
 
 export async function productsOnGroupRoutes(fastify: FastifyInstance) {
   fastify.get('/', (req: FastifyRequest, reply: FastifyReply) => {
     return new GetProductsOnGroupController().handle(req, reply)
-  })
-
-  fastify.get('/list', (req: FastifyRequest, reply: FastifyReply) => {
-    return new GetAllProductsOnGroupController().handle(req, reply)
   })
 
   fastify.post('/', (req: FastifyRequest, reply: FastifyReply) => {
